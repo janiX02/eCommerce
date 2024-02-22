1cf0dc39cd04c6e7a107c52866a9df46f023e489
Mikołaj Janik <mikolaj.j003@interia.pl>
2024-02-22 23:33:43 +0100

    Checkout form - dropdown lists with populating countries, states and dates works

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/firstangularproject/ecommerce/entity/Country.java
@@ -0,0 +1,31 @@
+package firstangularproject.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
+    @JsonIgnore // ignore the states retrieving data in JSON
+    private List<State> states;
+
+}
