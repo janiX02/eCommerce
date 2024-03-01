4b72dfa1f3035834e0c7db38f126bb83f64b34e7
Mikołaj Janik <mikolaj.j003@interia.pl>
2024-03-01 23:44:16 +0100

    Added configuration of cors

--- 02-backend/spring-boot-ecommerce/src/main/java/firstangularproject/ecommerce/dao/CountryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/firstangularproject/ecommerce/dao/CountryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://localhost:4200") // deprecated, now it's configured by cors in the configuration file
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
